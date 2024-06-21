b28219fdacba20f5128f4acbbaacaba82f6b41f8
Guilherme <gui_tsuna@hotmail.com>
2024-06-20 22:12:18 -0300

    Add BusinessListCard component

---
+++ components/BusinessList/BusinessListCard.jsx
@@ -0,0 +1,47 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+import { Colors } from '../../constants/Colors'
+
+export default function BusinessListCard({business}) {
+  return (
+    <View style={{
+      padding:10,
+      margin:10,
+      borderRadius:15,
+      backgroundColor:'#fff',
+      display: 'flex',
+      flexDirection: 'row',
+      gap: 10,
+      alignItems: 'center'
+    }}>
+      <Image source={{uri:business.imageUrl}}
+        style={{
+          width: 120,
+          height: 120,
+          borderRadius: 15
+        }}
+      />
+
+      <View style={{
+        flex: 1,
+        gap: 7
+      }}>
+        <Text style={{fontFamily:'outfit-bold', fontSize: 18}}>{business.name}</Text>
+        <Text style={{fontFamily:'outfit-medium', color:Colors.GRAY, fontSize: 15}}>{business.address}</Text>
+        <View style={{
+            display: 'flex',
+            flexDirection:'row',
+            gap: 5
+          }}>
+            <Image source={require('./../../assets/images/star.png')}
+              style={{
+                width:15,
+                height:15
+              }}
+            />
+            <Text style={{fontFamily:'outfit-medium'}}>4.5</Text>
+          </View>
+      </View>
+    </View>
+  )
+}
