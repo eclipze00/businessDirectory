70cde78eefaccc3082fbed15e31264aef862db3e
Guilherme <gui_tsuna@hotmail.com>
2025-01-20 18:06:51 -0300

    add BusinessID

---
+++ app/businessdetail/[businessid].jsx
@@ -0,0 +1,55 @@
+import { View, Text, ActivityIndicator } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { useLocalSearchParams } from 'expo-router'
+import { collection, getDoc, doc } from 'firebase/firestore';
+import { db } from '../../configs/FirebaseConfig';
+import { Colors } from '../../constants/Colors';
+import Intro from '../../components/BusinessDetail/Intro';
+
+export default function BusinessDetail() {
+
+  const {businessid}=useLocalSearchParams();
+  const [business, setBusiness]=useState();
+  const [loading, setLoading] = useState(false);
+
+    useEffect(()=>{
+      GetBusinessDetailById();
+    },[])
+
+  /**
+   * Usado para buscar o Id do Negócio
+   */
+  const GetBusinessDetailById=async()=>{
+    setLoading(true);
+    const docRef = doc(db, 'BusinessList', businessid);
+    const docSnap=await getDoc(docRef);
+    if (docSnap.exists()){
+      console.log ("Document Data:", docSnap.data());
+      setBusiness(docSnap.data());
+      setLoading(false);
+    } else{
+      console.log("Documento Inexistente");
+      setLoading(false);
+    }
+  }
+  return (
+    <View>
+      {loading?
+     <ActivityIndicator
+     style={{
+      marginTop: '70%'
+     }}
+        size={'large'}
+        color={Colors.PRIMARY}
+     />:
+     <View>
+        
+        {/**Intro*/}
+        <Intro business={business}/>
+
+     </View>
+    }
+      <Text>{businessid}</Text>
+    </View>
+  )
+}
