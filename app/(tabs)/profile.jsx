e515ba822a9331c62c7d09cb1c94fad45a318137
Guilherme <gui_tsuna@hotmail.com>
2025-01-24 13:25:17 -0300

    Adding the Profile screen.

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,10 +1,27 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import UserIntro from '../../components/Profile/UserIntro'
+import MenuList from '../../components/Profile/MenuList'
 
 export default function profile() {
   return (
-    <View>
-      <Text>profile</Text>
+    <View style={{
+      padding:20
+    }}>
+      <Text style={{
+        fontFamily:'outfit-bold',
+        fontSize: 35,
+        textAlign: 'center',
+        marginTop: 10
+      }}>Profile</Text>
+
+      {/* User Info */}
+
+      <UserIntro/>
+
+      {/* Menu list */}
+
+      <MenuList/>
     </View>
   )
 }
