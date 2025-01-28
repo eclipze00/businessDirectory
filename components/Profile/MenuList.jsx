75c5d728dab8524e990556f08551853c68b8372d
Guilherme <gui_tsuna@hotmail.com>
2025-01-28 18:02:07 -0300

    Adding the 'myBusiness', Share and Logout functionalities

--- components/Profile/MenuList.jsx
+++ components/Profile/MenuList.jsx
@@ -1,9 +1,12 @@
-import { View, Text, Image, FlatList, TouchableOpacity } from 'react-native'
+import { View, Text, Image, FlatList, TouchableOpacity, Share } from 'react-native'
 import React from 'react'
 import { Colors } from '../../constants/Colors';
 import { useRouter } from 'expo-router';
+import { useAuth } from '@clerk/clerk-expo';
 
 export default function MenuList() {
+
+  const {signOut}=useAuth();
 
   const menuList=[
     {
@@ -16,25 +19,37 @@ export default function MenuList() {
       id:2,
       name: 'My Business',
       icon:require('./../../assets/images/business-and-trade.png'),
-      path:''
+      path:'/business/myBusiness'
     },
     {
       id:3,
       name: 'Share App',
       icon:require('./../../assets/images/share_1.png'),
-      path:''
+      path:'share'
     },
     {
       id:4,
       name: 'Logout',
       icon:require('./../../assets/images/logout.png'),
-      path:''
+      path:'logout'
     }
   ]
 
   const router=useRouter();
 
   const onMenuClick=(item)=>{
+    if(item.path=='logout')
+    {
+      signOut();
+      return;
+    }
+    if(item.path=='share')
+    {
+      Share.share({
+        message:'Faça o dowload do App by Eclipze!'
+      })
+      return;
+    }
     router.push(item.path)
   }
   return (
